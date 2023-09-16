638bd1fa70a6c9b7a960bfc172062330c0a40a30
Ayush <ayushmishra2854@gmail.com>
2023-09-16 17:10:10 +0530

    document saving feature done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 import { Server } from "socket.io";
+import connectDB from "./database/db.js";
+import { config } from "dotenv";
+import { updateDocument, getDocuments } from "./controller/document-controller.js";
 
 const io = new Server(9000, {
     cors: {
@@ -7,15 +10,21 @@ const io = new Server(9000, {
         methods: ["GET", "POST"],
     },
 });
+config();
+connectDB();
 
 io.on("connection", (socket) => {
-    socket.on('get-document', (documentId) => {
-        const data = '';
+    socket.on('get-document', async (documentId) => {
+        const document = await getDocuments(documentId);
         socket.join(documentId);
-        socket.emit('load-document', data);
+        socket.emit('load-document', document.content);
 
         socket.on('send-changes', (delta) => {
             socket.broadcast.to(documentId).emit('receive-changes', delta);
+        });
+
+        socket.on('save-document', async (data) => {
+            updateDocument(documentId, data);
         });
     });
 });
